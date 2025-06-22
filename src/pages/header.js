2bd461256d54ae6af7b46af10efbf581819c8087
valerieyichu <valeriechu2026@u.northwestern.edu>
2025-06-22 16:42:34 -0700

    test deploy workflow

--- src/pages/header.js
+++ src/pages/header.js
@@ -2,9 +2,11 @@ import React from 'react';
 import '../styles/header.less';
 
 const Header = () => {
-    return(
-        <></>
-    )
-}
+  return (
+    <>
+      <h1>Valerie Chu</h1>
+    </>
+  );
+};
 
 export default Header;
