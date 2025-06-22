311d6e46c0758d0f47ce937b26b09b3e0bcb15be
valerieyichu <valeriechu2026@u.northwestern.edu>
2025-06-22 14:36:55 -0700

    divider made

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../styles/home.less';
+import divider from '../images/divider.svg';
 
 const Bio = () => {
   return (
     <>
       <div className='centered-text'>
         <h1 className='h1'>Hi! I'm Valerie and I love storytelling.</h1>
-        <h2 className='h2'>I’m an aspiring digital designer and front-end web developer. I also wrangle data, photograph and write.</h2>
+        <h2 className='h2'>
+          I’m an aspiring digital designer and front-end web developer. I also
+          wrangle data, photograph and write.
+        </h2>
       </div>
+      <img src={divider} alt='divider' className='divider' />
     </>
   );
 };
