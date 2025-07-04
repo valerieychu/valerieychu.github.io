5bc0e93c9c429106a8370dbc8eda1898cb12df28
valerieyichu <valeriechu2026@u.northwestern.edu>
2025-07-04 12:26:00 -0700

    intro made

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../styles/global.less';
+import '../styles/home.less';
+import image from '../images/valeriechu.png';
 import divider from '../images/divider.svg';
 
 const Bio = () => {
   return (
     <>
-      <div className='centered-text'>
-        <h1 className='h1'>Hi! I'm Valerie and I love storytelling.</h1>
-        <h2 className='h2'>
-          I’m an aspiring digital designer and front-end web developer. I also
-          wrangle data, photograph and write.
-        </h2>
+      <div className='intro'>
+        <div className='centered-text'>
+          <h1 className='h1'>Hi! I'm Valerie and I love storytelling.</h1>
+          <h2 className='h2'>
+            I’m an aspiring digital designer and front-end web developer. I also
+            wrangle data, photograph and write.
+          </h2>
+        </div>
+        <img className='image' src={image} alt='Image of Valerie Chu' />
       </div>
       <img src={divider} alt='divider' className='divider' />
     </>
