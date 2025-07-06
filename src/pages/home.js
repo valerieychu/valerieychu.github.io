47148552f3e0bf91783e7fcb2b60cdacb462e51d
valerieyichu <valeriechu2026@u.northwestern.edu>
2025-07-06 11:21:26 -0700

    scoreboard project written

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ import '../styles/home.less';
 import image from '../images/valeriechu.png';
 import divider from '../images/divider.svg';
 
-const Bio = () => {
+const Home = () => {
   return (
     <>
       {/* Intro */}
@@ -20,8 +20,8 @@ const Bio = () => {
             several other places. I've had experience in a variety of roles
             including digital design/development, data analytics, database
             marketing, political reporting and social media. Now, I want to use
-            those skills to dive into the intersection of journalism, data
-            science and design/development where storytelling thrives.
+            those skills to dive into storytelling at the intersection of data
+            science, journalism and design/development.
           </h2>
         </div>
         <img className='image' src={image} alt='Image of Valerie Chu' />
@@ -31,23 +31,24 @@ const Bio = () => {
       <div className='centered-text'>
         <h3 className='h3'>Select Works</h3>
         <h4 className='h4'>San Francisco Chronicle — Scoreboard Project</h4>
-        <p>
-          The San Francisco Chronicle produced two photo stories that portrayed
+        <p className='paragraph'>
+          The San Francisco Chronicle published two photo stories that portrayed
           key moments from the Golden State Warriors' games in the first round
           then the semifinals of the NBA playoffs. Since each story featured
           photos taken during the entire round (ie. multiple games), we wanted
           to allow readers to visually track which game they're looking at. As
           the designer/developer for the project, I created a custom scoreboard
           component.
-          <br />
-          <br />
+        </p>
+
+        <p className='paragraph'>
           {/* <span style={{ fontWeight: 'bold' }}>Tools used:</span>  */}
           <i>Tools used: </i>
           React, Figma, ArchieML, HTML, CSS, JavaScript
-          <br />
-          <br />
+        </p>
+
+        <p className='paragraph'>
           <i>Process: </i>
-          <br />
           <br />
           When creating the scoreboard component, I wanted to start by
           considering the history of old-school sports design. My mentor, Alex
@@ -56,29 +57,64 @@ const Bio = () => {
           through some of their work as well as that of other designers, I began
           to pick up on particular ways of displaying fundamental sports
           information — with scoreboards being one such idea. Then my goal
-          became to take these ideas I had from print and make them digitally
-          presentable. I used Figma to design multiple scoreboard options,
-          ultimately landing on one that could be reused for other sports
-          including the WNBA, MLB and NFL.
-          <br />
-          <br />
+          became to take the inspiration I gained from print sports design and
+          make those ideas digitally presentable. I used Figma to design
+          multiple scoreboard options, ultimately landing on one that could be
+          reused for other sports including the WNBA, MLB and NFL.
+        </p>
+        <p className='paragraph'>
           The scoreboard’s dynamic design adjusts to mobile and desktop devices,
           and allows editors and writers to simply edit an ArchieML tag in
           Google Docs to change scoreboard components — team names, score and
           game number — without needing access to the code or coding skills. The
           scoreboard then auto-designs itself.
-          <br />
-          <br />
+        </p>
+        <p className='paragraph'>
           This scoreboard component was deployed for two Golden State Warriors
           photo stories. Since each photo story was published immediately after
           each NBA round, I utilized the scoreboard component and also
           designed/placed the game photos into each story under deadline. The
-          scoreboard component exists as a San Francisco Chronicle
-          component and will be reused for other sports we cover in the future.
+          scoreboard component exists as a San Francisco Chronicle component and
+          will be reused for other sports we cover in the future.
         </p>
+
+        <button
+          className='view-this-project'
+          style={{ marginRight: '1rem' }}
+          onClick={() =>
+            window.open(
+              'https://www.sfchronicle.com/projects/2025/warriors-rockets-nba-playoffs/',
+              '_blank',
+            )
+          }>
+          View this project (1st story)
+        </button>
+
+        <button
+          className='view-this-project'
+          onClick={() =>
+            window.open(
+              'https://www.sfchronicle.com/projects/2025/warriors-timberwolves-nba-playoffs/',
+              '_blank',
+            )
+          }>
+          View this project (2nd story)
+        </button>
+
+        <video
+          width='640'
+          height='360'
+          controls
+          // autoPlay
+          muted
+          loop
+          style={{ maxWidth: '100%', borderRadius: '8px', marginBottom: '1rem' }}>
+          <source src='/scoreboard-component.mp4' type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </>
   );
 };
 
-export default Bio;
+export default Home;
