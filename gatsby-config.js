d023761e22e4b54425f3bac5636809b7ffee124f
Valerie Chu <valerieyichu@gmail.com>
2025-11-12 00:45:43 -0600

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/valeriechu.png",
       },
     },
     "gatsby-plugin-mdx",
